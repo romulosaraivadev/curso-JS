bc518b6b56c03c79ed41c7c9ebddcfd2b0f3b81f
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-06-17 07:50:52 -0300

    funçoies

---
+++ aula 10/index.js
@@ -0,0 +1,27 @@
+/**
+ * aritméticos 
+ * + adição / concatenação (- / *)
+ * ** potenciação
+ * % resto da divisão
+*/
+
+/**
+ * precedência
+    ()
+    **
+    * / %
+    * + -
+ */
+
+/*
+incremento = ++
+decremento = --
+
+
+*/
+
+// NaN = not a number
+
+
+const num1 = 10
+const num2 = parseInt('5')
---
+++ aula 9/index.js
@@ -0,0 +1,18 @@
+//string, number, undefined, null, boolean
+const nome = 'romulo' //string
+const nome1 = "romulo" //string
+const nome2 = `romulo` //string
+const num1 = 10 //number
+const num2 = 10.52  //nubem
+let numeAluno  //undefined -> não aponta para nenhum local na memória
+let sobrenomeAluno = null //nulo -> não aponta para nenhum local na memória
+const aprovado = false //boolean = true, false (lógico)
+
+let a = 2
+const b = a
+
+console.log (a, b)
+
+a = 3
+
+console.log (a, b)
