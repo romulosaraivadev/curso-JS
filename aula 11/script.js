cdf0da2aee82bfd0023f2e415ac345c1bde11e5c
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-06-17 13:36:16 -0300

    ~j

---
+++ aula 11/script.js
@@ -0,0 +1,7 @@
+let num1 = prompt('digite um numero:');
+let num2 = prompt('digite outro numero:');
+
+num1 = parseFloat(num1)
+num2 = Number(num2)
+
+alert(`O resultador da sua conta foi: ${num1 + num2}`)
