21aced4bec63c6bc33cfe77e2b59f8f3b4294c0d
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-06-19 13:32:06 -0300

    array

---
+++ aula 16/index.js
@@ -0,0 +1,34 @@
+//indice      012345678910
+// const nome = 'Rômulo Saraiva'
+// console.log(nome[1])
+//indece           0       1       2
+const alunos = ['Luiz', 'Maria', 'João']; 
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+
+// console.log(alunos.slice(0, -1));
+
+// delete alunos[1] remover algum indice
+
+// const removido = alunos.shift() remove o primeiro elemento
+
+// const removido = alunos.pop() remove do final
+
+// alunos.unshift('Luiza'); adiciona no começo
+// alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); adiciona ao final
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza'; adicionar ao final
+// alunos[alunos.length] = 'Romulo';
+// alunos[alunos.length] = 'Andressa';
+
+// alunos[0] = 'Eduardo'; para alterar
+
+// alunos[3]= 'luiza'; adiciona
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
