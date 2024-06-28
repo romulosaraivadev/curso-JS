e81add8062f43c7b356c52ece0a699a83f46b0c2
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-06-28 18:32:00 -0300

    exercicio

--- aula 35/exercicio/assets/js/main.js
+++ aula 35/exercicio/assets/js/main.js
@@ -1,3 +1,31 @@
+// const elementos = [
+//     {tag: 'p', texto: 'frase 1'},
+//     {tag: 'div', texto: 'frase 2'},
+//     {tag: 'footer', texto: 'frase 3'},
+//     {tag: 'section', texto: 'frase 4'},
+
+// ];
+
+// const container = document.querySelector('.container');
+// const div = document.createElement('div'); //serve para criar elementos
+
+// for (let i = 0; i < elementos.length; i++){
+//     let {tag, texto} = elementos[i];
+//     let tagCriada = document.createElement(tag)
+//     let textoCriado = document.createTextNode(texto)
+//     // tagCriada.innerText = texto;
+//     tagCriada.appendChild(textoCriado)
+//     div.appendChild(tagCriada);
+// }
+// container.appendChild(div);
+
+
+
+
+
+
+
+
 const elementos = [
     {tag: 'p', texto: 'frase 1'},
     {tag: 'div', texto: 'frase 2'},
@@ -7,12 +35,20 @@ const elementos = [
 ];
 
 const container = document.querySelector('.container');
-const div = document.createElement('div'); //serve para criar elementos
+const div = document.createElement('div')
 
 for (let i = 0; i < elementos.length; i++){
-    let {tag, texto} = elementos[i];
-    let tagCriada = document.createElement(tag)
-    tagCriada.innerText = texto;
-    div.appendChild(tagCriada);
+    let {tag, texto} = elementos[i]
+    let criaTag = document.createElement(tag)
+    let criarTexto = document.createTextNode(texto)
+
+ criaTag.appendChild(criarTexto)
+ div.appendChild(criaTag)
+
 }
-container.appendChild(div);
+container.appendChild(div)
+
+
+
+
+
