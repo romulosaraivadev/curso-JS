a6666f1691333ed459abafcf4b42a7f3e63eecfc
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-07-04 12:25:56 -0300

    exercicio lista de tarefas

--- aula 47/meu exercicio/assets/js/main.js
+++ aula 47/meu exercicio/assets/js/main.js
@@ -1,14 +1,43 @@
-const relogio = document.querySelector('.relogio')
-const iniciar = document.querySelector('.iniciar')
-const pausar = document.querySelector('.pausar')
-const zerar = document.querySelector('.zerar')
+function relogio() {
+function criaHoraDosSegundos(segundos){
+    const data = new Date(segundos * 1000)
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'UTC'
+    })
+};
+const relogio = document.querySelector('.relogio');
+let segundos = 0;
+let timer;
 
-iniciar.addEventListener('click', function(event) {
-    alert('cliquei em iniciar')
+function iniciaRelogio() {
+    timer = setInterval(function() {
+        segundos++;
+        relogio.innerHTML = criaHoraDosSegundos(segundos)
+    }, 1000)
+}
+
+document.addEventListener('click', function(e) {
+  const el = e.target;
+  console.log(el)
+
+  if(el.classList.contains('iniciar')) {
+      relogio.classList.remove('pausado')
+      clearInterval(timer)
+      iniciaRelogio()
+    }
+    if(el.classList.contains('pausar')) {
+        relogio.classList.add('pausado')
+        clearInterval(timer)
+    }
+    
+    if(el.classList.contains('zerar')) {
+      relogio.classList.remove('pausado')
+      clearInterval(timer);
+      relogio.innerHTML = "00:00:00"
+      segundos = 0;
+    }
 })
-pausar.addEventListener('click', function(event) {
-    alert('cliquei em pausar')
-})
-zerar.addEventListener('click', function(event) {
-    alert('cliquei em zerar')
-})
+}
+
+relogio()
