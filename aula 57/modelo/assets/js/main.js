f232db9f193a1b4f19d07fabd723c0f963e57d06
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-07-11 19:19:23 -0300

    criando uma calculadora

--- aula 57/modelo/assets/js/main.js
+++ aula 57/modelo/assets/js/main.js
@@ -2,17 +2,72 @@ function criaCalculadora() {
     return {
         //atributos
         display: document.querySelector('.display'),
-
-
+        btnClear: document.querySelector('btn-clear'),
         inicia() {
-            alert('oi, iniciei')
+            this.cliqueBotoes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', e => {
+                if (e.keyCode === 13) {
+                    this.realizaConta();
+                }
+            })
+        },     
+
+        realizaConta() {
+            let conta = this.display.value;
+
+            try {
+                conta = eval(conta);
+
+                if(!conta){
+                    alert('Conta invalida')
+                    return;
+                }
+
+                this.display.value = String(conta);
+            } catch(e) {
+                alert('Conta invalida');
+                return
+            }
+        },
+        
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1);
         },
         //metodos
         cliqueBotoes() {
-            document.addEventListener('click', function(e) {
+            // this -> calculadora
+            document.addEventListener('click', e => {
+                const el = e.target;
                 
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if(el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
             })
         },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
 
     };
 }
