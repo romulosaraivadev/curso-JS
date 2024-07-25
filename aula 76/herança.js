5b5bbdfd955201b79fc5e1ce347b7eaae8fa47dc
romulosaraivadev <167458339+romulosaraivadev@users.noreply.github.com>
2024-07-25 19:54:12 -0300

    herança

--- aula 76/herança.js
+++ aula 76/herança.js
@@ -13,9 +13,41 @@ Produto.prototype.desconto = function(quantia) {
 
 function Camisata(nome, preco, cor) {
     Produto.call(this, nome, preco)
+    this.cor = cor;
 }
-Camisata.prototype = Object.create(Produto.prototype)
 
+Camisata.prototype = Object.create(Produto.prototype)
+Camisata.prototype.constructor = Camisata;
+
+Camisata.prototype.aumento = function(perentual) {
+    this.preco = this.preco + (this.preco * (perentual / 100))
+}
+
+function Caneca(nome, preco, material, estoque) {
+    Produto.call(this, nome, preco);
+    this.material = material
+    
+    Object.defineProperty(this, 'estoque', {
+        enumerable: true,
+        configurable: false,
+        get: function() {
+            return estoque
+        },
+        set: function(valor) {
+            if (typeof valor !== 'number') return
+            estoque = valor
+        }
+    })
+}
+Caneca.prototype = Object.create(Produto.prototype);
+Caneca.prototype.constructor = Caneca;
+
+const produto = new Produto('Gen', 111)
 const camiseta = new Camisata('Regata', 7.5, 'Preta')
-camiseta.aumento(10)
+const caneca = new Caneca('Caneca', 13, 'Plastico', 5)
+caneca.estoque = 100
+
+console.log(caneca)
+console.log(caneca.estoque)
 console.log(camiseta)
+console.log(produto)
